666e16d3f97f57ad57f3100bdb8306c5f8b18154
Daniil Loban <dungeon_lda@mail.ru>
2018-09-11 05:49:30 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,31 +1,59 @@
+
 class Sorter {
+
+  
   constructor() {
-    // your implementation
+      this.array = [];
+      this.DefaultComparator = (a, b) => a - b;
+      this.compareFunction = this.DefaultComparator
   }
 
   add(element) {
-    // your implementation
+      this.array.push(element); 
   }
 
   at(index) {
-    // your implementation
+      return this.array[ index ];
   }
 
   get length() {
-    // your implementation
+      return this.array.length;
   }
 
   toArray() {
-    // your implementation
+      return this.array.slice();
   }
 
-  sort(indices) {
-    // your implementation
-  }
+  sort(indices) {  
+    
+    var temp =[];
+    indices.sort(this.defaultCompareFunction);
+
+    for (var i = 0; i < indices.length; i++) {
+        if (indices[i] >= 0 && indices[i] < this.array.length){
+            temp.push(this.array[ indices[i] ]);
+        }
+        else{
+            console.log("Index: " + indices[i] + " outs of range!");
+        }
+    }
+    
+    var tempSize = temp.length;
+    temp.sort(this.compareFunction);
+    
+    for (var i = 0; i < tempSize; i++) {
+        this.array[ indices[i] ] = temp.shift();
+    };
+  };
 
   setComparator(compareFunction) {
-    // your implementation
-  }
-}
+      if (compareFunction != null){
+          this.compareFunction = compareFunction;
+      } else {
+          this.compareFunction = this.DefaultComparator
+      }
+  };
+
+};
 
 module.exports = Sorter;
