c07a143a801e1d8d6211e2cd09214fb91913dfba
AntiHero <prophreaker@gmail.com>
2018-09-21 16:35:35 +0300

    sorter: task completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 class Sorter {
     constructor() {
-        this.arr = new Array();
+        this.arr = [];
+        this.compareFunction = (a, b) => a - b;
     }
 
     get length() {
@@ -12,26 +13,50 @@ class Sorter {
     }
 
     at(index) {
-        // your implementation
+        return this.arr[index];
     }
 
     toArray() {
-        
+        return this.arr;
     }
 
     sort(indices) {
-        // your implementation
+        let sortedPart = [];
+        indices.sort((a, b) => a - b);
+
+        for (let i in indices) {
+            sortedPart.push(this.arr[indices[i]]);
+        }
+
+        sortedPart.sort(this.compareFunction);
+
+        for (let i = 0; i < indices.length; i++) {
+            this.arr[indices[i]] = sortedPart[i];
+        }
+        return this.arr;
     }
 
     setComparator(compareFunction) {
-        // your implementation
+        compareFunction == undefined ? null : this.compareFunction = compareFunction;
     }
 }
 
 
-let sorter = new Sorter();
-console.log(sorter.length);
+/*let sorter = new Sorter();
 sorter.add(5);
+sorter.add(4);
 sorter.add(3);
+console.log(sorter.toArray());
+sorter.sort([0, 1, 2]);
+console.log(sorter.toArray());
 console.log(sorter.length);
-//module.exports = Sorter;
+console.log(typeof sorter);
+sorter.add(1);
+sorter.add(2);
+sorter.add(3);
+sorter.sort[0, 1, 2];
+sorter.sort([0]);
+console.log(sorter.toArray());
+console.log(sorter.toArray() == [1])*/
+
+module.exports = Sorter;
